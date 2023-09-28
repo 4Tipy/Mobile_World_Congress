6dcf712d30a147dfc7f95a2496691ec21d89cdd1
Eric <eric.sanchez.vazquez04@gmail.com>
2023-09-28 15:16:30 +0200

    main

--- primerProyecto/mobile_world_congress/src/App.java
+++ primerProyecto/mobile_world_congress/src/App.java
@@ -15,15 +15,15 @@ public class App {
         List<Dispositivos> disp = new ArrayList<>();
 
         // un rellotge intel·ligent de 100 Euros
-        disp.add(new otros_disp("prueba1", "prueba2", 100, "reloj inteligente") );
+        disp.add(new otros_disp("Xiaomi", "2", 100, "reloj inteligente") );
         // un mòbil Android sense accelerador ni gps de 300 Euros
-        disp.add(new smartphones("sdas", "klnfsdlds", 300, "Android", false, false));
+        disp.add(new smartphones("Samsung", "galaxy12", 300, "Android", false, false));
         // un iPhone de 500 Euros amb accelerador i gps
-        disp.add(new smartphones("iphone", "klsdn", 500, "dsl fds", true, true));
+        disp.add(new smartphones("iphone", "8", 500, "IOS", true, true));
         //una tablet de 400 Euros i un altre dispositiu qualsevol
-        disp.add(new Tablets("lksdmgkfd", "kjdnbgjkdf", 400, 0));
+        disp.add(new Tablets("Xiaomi", "pad5", 400, 0));
 
-        disp.add(new otros_disp("prueba4", "prueba5", 600, "patata") );
+        disp.add(new otros_disp("OPO", "5", 600, "patata") );
 
         for(Dispositivos lista : disp){
             System.out.println(lista);
